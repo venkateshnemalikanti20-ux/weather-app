0d01cf3acec993bc14c4acd59ecba45dad47dcc4
venkateshnemalikanti20-ux <venkateshnemalikanti20@gmail.com>
2026-07-20 18:53:28 +0530

    small changes on server

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,32 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const axios = require("axios");
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const axios = require("axios")
 
-// Load environment variables
-dotenv.config();
-console.log("API KEY:", process.env.WEATHER_API_KEY);
+dotenv.config()
 
-// Create Express App
-const app = express();
+const app = express()
 
-// Enable CORS
-app.use(cors());
 
-// Read values from .env
-const PORT = process.env.PORT || 3000;
-const API_KEY = process.env.WEATHER_API_KEY;
-const GEOAPIFY_API_KEY = process.env.GEOAPIFY_API_KEY;
+app.use(cors())
 
-// Home Route
+const PORT = process.env.PORT || 3000
+const API_KEY = process.env.WEATHER_API_KEY
+const GEOAPIFY_API_KEY = process.env.GEOAPIFY_API_KEY
+
+
 app.get("/", (req, res) => {
     res.send("Weather Backend Server Running");
-});
+})
 
-// Weather Route
+
 app.get("/weather", async (req, res) => {
     const city = req.query.city;
 
     try {
         const response = await axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-        );
+        )
 
         res.json({
     city: response.data.name,
@@ -39,41 +35,33 @@ app.get("/weather", async (req, res) => {
     wind: response.data.wind.speed,
     feelsLike: response.data.main.feels_like,
     description: response.data.weather[0].description
-});
+})
 
     } catch (error) {
-    console.log("========== ERROR ==========");
-    console.log(error.response?.status);
-    console.log(error.response?.data);
-    console.log(error.message);
-    console.log("===========================");
-
+    
     res.status(500).json({
         message: "Unable to fetch weather data"
-    });
+    })
 }
-});
-// Suggestions Route
+})
+
 app.get("/suggestions", async (req, res) => {
     const city = req.query.city;
 
     try {
         const response = await axios.get(
             `https://api.geoapify.com/v1/geocode/autocomplete?text=${city}&apiKey=${GEOAPIFY_API_KEY}`
-        );
+        )
 
-        res.json(response.data);
+        res.json(response.data)
 
     } catch (error) {
-        console.log("Geoapify Error:", error.response?.data || error.message);
-
         res.status(500).json({
             message: "Unable to fetch suggestions"
-        });
+        })
     }
-});
+})
 
-// Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
